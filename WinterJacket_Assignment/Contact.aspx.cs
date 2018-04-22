6083bdb61b2649c8acc59217dbffd25ca5404360
adlen <nuradhwaadlen.sapawi@gmail.com>
2018-04-23 00:26:37 +0800

    web.config

--- WinterJacket_Assignment/Contact.aspx.cs
+++ WinterJacket_Assignment/Contact.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Net.Mail;
 
 namespace WinterJacket_Assignment
 {
@@ -11,12 +12,39 @@ namespace WinterJacket_Assignment
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //Sends email using a mail server that requires login credentials and a secure
+           
         }
 
         protected void btnSendEmail_Click(object sender, EventArgs e)
         {
+            //Sends email using a mail server that requires login credentials and a secure, e.g. gmail
 
+            //Create mail client and message with to and from address, and set message subject and body 
+            SmtpClient smtpClient = new SmtpClient();
+            MailMessage msg = new MailMessage("nuradhwaadlen.sapawi@gmail.com", "nuradhwaadlen.sapawi@gmail.com");
+            msg.Subject = txtSubject.Text;
+            msg.Body = txtBody.Text;
+
+            //Settings specific to the mail service, e.g. server location, port number and that ssl is required
+            smtpClient.Host = "smtp.gmail.com";
+            smtpClient.Port = 587;
+            smtpClient.EnableSsl = true;
+
+            //Create credentials - e.g. username and password for the account
+            System.Net.NetworkCredential credentials = new System.Net.NetworkCredential("nuradhwaadlen.sapawi@gmail.com", "password");
+            smtpClient.Credentials = credentials;
+
+            try
+            {
+                smtpClient.Send(msg);
+                litResult.Text = "<p>Success, mail sent using SMTP with secure connection and credentials</p>";
+            }
+            catch (Exception ex)
+            {
+                //Display the full error to the user
+                litResult.Text = "<p>Send failed: " + ex.Message + ":" + ex.InnerException + "</p>";
+
+            }
         }
     }
 }
